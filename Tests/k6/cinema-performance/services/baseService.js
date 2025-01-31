446be09f822f695731ffc3b27a5ee0795101431c
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-01-31 19:15:14 -0300

    Removendo arquivos desnecessários da branch ci-cd

--- Tests/k6/cinema-performance/services/baseService.js
+++
@@ -1,8 +0,0 @@
-import { ENVIRONMENTS } from '../support/config/Environments.js'
-const DEFAULT_BASE_URI = ENVIRONMENTS.LOCAL;
-
-export class BaseService {
-    constructor(base_uri) {
-        this.base_uri = base_uri || DEFAULT_BASE_URI;
-    }
-}
