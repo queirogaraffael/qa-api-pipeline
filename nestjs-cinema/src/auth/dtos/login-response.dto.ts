debc3e2460a5eb610b36d9b718eac6922bb9fff3
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-08-22 21:19:30 -0300

    reestruturação

--- nestjs-cinema/src/auth/dtos/login-response.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginResponse {
-  @ApiProperty({ description: 'Token JWT gerado após login bem-sucedido' })
-  access_token: string;
-}
--- nestjs-cinema/src/exceptions/email-not-found.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class EmailNotFoundException extends HttpException {
-  constructor() {
-    super('Email não encontrado.', HttpStatus.NOT_FOUND);
-  }
-}
